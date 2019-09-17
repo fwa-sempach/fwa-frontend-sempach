0bf97e6eccba049e77a94131572a12a5bfb59b09
Unknown <mrvonwyl@gmail.com>
2019-09-17 17:38:59 +0200

    prod api changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,8 @@
 export const environment = {
   production: true,
   // apiUrl: 'http://localhost:8080/api/v1'
-  apiUrl: 'https://freiwilligenarbeit-sempach.ch/api/v1',
+  apiUrl: 'https://api.meggen-freiwillige.ch/api/v1',
   quillOptions: {
-    toolbar: [
-      ['bold'],
-      [{ 'list': 'bullet' }]
-    ]
+    toolbar: [['bold'], [{ list: 'bullet' }]]
   }
 };
