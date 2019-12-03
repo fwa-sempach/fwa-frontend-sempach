1585023569f129a712247108042402ede16619a4
Marcel von Wyl <mrvonwyl@gmail.com>
2019-12-03 11:34:03 +0100

    prod api changed to staging-api.fwa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // apiUrl: 'http://localhost:8080/api/v1'
-  apiUrl: 'https://api.meggen-freiwillige.ch/api/v1',
+  apiUrl: 'https://staging-api.freiwilligenarbeit-sempach.ch/api/v1',
   quillOptions: {
     toolbar: [['bold'], [{ list: 'bullet' }]]
   }
