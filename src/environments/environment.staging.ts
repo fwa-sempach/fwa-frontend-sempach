6a9027aebd34453d971ea6c9685b441eb64bc23b
Marcel von Wyl <mrvonwyl@gmail.com>
2019-12-04 13:52:11 +0100

    env and dockerfile for staging

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://staging-api.freiwilligenarbeit-sempach.ch/api/v1',
+  quillOptions: {
+    toolbar: [['bold'], [{ list: 'bullet' }]]
+  }
+};
